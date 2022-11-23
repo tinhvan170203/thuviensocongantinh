7f1f25e6b50e93e227af1ca8642a69897e9b9c5b
unknown <vuvantinh121123@gmail.com>
2022-11-23 09:05:48 +0700

    lan 2

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(function(req, res, next) {
     next();
   });
 app.use(cors({
-    origin: "https://conganhungyenthuvienso.onrender.com",
+    origin: "https://thuviensoconganhungyen.onrender.com",
     credentials: true
 }));
 app.use(express.json());
